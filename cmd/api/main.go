a35b9cf0320b8844d7e7b33ee3008e526a2a9d88
Kayode Odole <31928403+Kayconfig@users.noreply.github.com>
2026-01-27 00:49:01 +0100

    feat: configure database connection pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
@@ -22,13 +33,33 @@ type application struct {
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DATABASE_CONNECTION_STRING"), "PostgreSQL DSN")
+
+	// Read the connection pool settings from command-line flags into the config struct.
+	// Notice that the default values we're using are the ones we discussed above?
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max  connection idle time")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -50,7 +81,43 @@ func main() {
 	// start HTTP server
 	logger.Info("starting server", "addr", server.Addr, "env", cfg.env)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	// set the maximum number of open (in-use + idle) connections in the ppol.
+	// a value less than or equal to zero will mean there is no limit
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+	//Set the max number of idle connections in the pool.
+	// value less than or equal to zero mean there is no limit
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	// set the maximum idle timeout for connections in the pool.
+	// duration less than or equal to zero will mean that the
+	// connectinos are not closed due to their idle time
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	delayInSeconds := 5 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), delayInSeconds)
+	defer cancel()
+
+	//use pingContext to establish new connection to db,
+	//passing in the context we created above. if connection
+	// couldn't be established within the specified delayInSeconds
+	// the following will return an error
+	// if we get error, we close the connection pool and return error
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.25.3
 
 require (
 	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 )
