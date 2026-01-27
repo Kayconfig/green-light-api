4cf4e6e9a3322554bd0a98cf01ddfe07b2f534d5
Kayode Odole <31928403+Kayconfig@users.noreply.github.com>
2026-01-27 20:22:44 +0100

    feat: add edit conflict handling for movie updates

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -52,3 +52,8 @@ func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.
 func (app *application) unprocessableEntityResponse(w http.ResponseWriter, r *http.Request, msg string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, msg)
 }
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
