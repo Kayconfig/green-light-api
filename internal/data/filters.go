da15ff41637ffea4c9948fe60fcaced873625a81
Kayode Odole <31928403+Kayconfig@users.noreply.github.com>
2026-01-28 04:25:48 +0100

    feat: enhance movie retrieval with pagination metadata and add database indexes

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/kayconfig/green-light-api/internal/validator"
+import (
+	"strings"
+
+	"github.com/kayconfig/green-light-api/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -9,10 +13,70 @@ type Filters struct {
 	SortSafeList []string
 }
 
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitzero"`
+	PageSize     int `json:"page_size,omitzero"`
+	FirstPage    int `json:"first_page,omitzero"`
+	LastPage     int `json:"last_page,omitzero"`
+	TotalRecords int `json:"total_records,omitzero"`
+}
+
+// The calculateMetadata() function calculates the appropriate pagination metadata
+// values given the total number of records, current page, and page size values.
+// Note that when the last page is calculated we are dividing by two int values, and
+// when dividing integers types in Go the result will also be an integer type, with
+// the modulus (or remainder) dropped. So, for example, if there were 12 records in total
+// and a page size of 5, the last page value would be (12 + 5 -1) / 5 = 3.2, which is then
+// trucated to 3 by Go
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		// Note that we return an empty Metadata struct if there are no records
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
+}
+
 func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+// Check that the client-provided Sort field matches one of the entries in our safelist
+// and if it does, extract the column name from the Sort field by stripping the leading
+// hyphen character (if one exists).
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+// Return the sort direction ("ASC" or "DESC") depending on the prefix character
+// of the Sort field
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
